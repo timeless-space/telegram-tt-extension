adb3f1d349f6a005cfe56fe1cc5e686434bc1822
Giang Bui <giang.bui@zien.vn>
2023-08-08 10:25:10 +0700

    Summary: Fix change padding top issue with points unit in iOS

--- src/util/tlCustomFunction.ts
+++ src/util/tlCustomFunction.ts
@@ -2,11 +2,34 @@
  * TL - Add padding top
  * Description: Add padding top when called this function, all elements have 'tl-custom-padding' className will be change styles.
  */
+import { getActions } from '../global';
+import type { Message } from '../global/types';
 
-export function changePaddingTopMobile(padding: string) {
-  const element1: any = document.getElementById('left-main-header');
+export function changePaddingTopMobile(padding = 0) {
+  const element1: any = document.getElementById('fix-issue-mobile');
   const element2: any = document.getElementById('custom-id-chat-list-inf-scroll');
 
-  element1.style.paddingTop = `${Number(padding)}px`;
-  element2.style.paddingTop = `${Number(padding)}px`;
+  element1.style.paddingTop = `${padding}px`;
+  element2.style.paddingTop = `${padding}px`;
+}
+
+/**
+ * TL - Custom a sendMessage function to send a message
+ */
+export function handleSendMessage({ chatId, threadId = 0, text }: Message) {
+  getActions().sendMessage({
+    text,
+    messageList: {
+      chatId,
+      threadId,
+      type: 'thread',
+    },
+  });
+}
+
+/**
+ * TL - Set session screen name
+ */
+export function sendScreenName(name: string) {
+  (window as any).webkit?.messageHandlers.onScreenChanged.postMessage({ screenName: name });
 }
